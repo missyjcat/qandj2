35309e05dde484572d882ac8058fa3febceeaa74
Jessica Chan <jessicachan@squaretrade.com>
2013-11-30 17:13:56 -0800

    page--front adding

---
+++ page--front.tpl.php
@@ -0,0 +1,50 @@
+<?php
+//kpr(get_defined_vars());
+//kpr($theme_hook_suggestions);
+//template naming
+//page--[CONTENT TYPE].tpl.php
+?>
+<?php if( theme_get_setting('mothership_poorthemers_helper') ){ ?>
+<!-- page.tpl.php-->
+<?php } ?>
+
+<?php print $mothership_poorthemers_helper; ?>
+
+<div class="page">
+
+  <?php if ($page['sidebar_first']): ?>
+    <div class="sidebar-first">
+    <?php print render($page['sidebar_first']); ?>
+    </div>
+  <?php endif; ?>
+
+  <div role="main" id="main-content">
+
+    <?php if ($action_links): ?>
+      <ul class="action-links"><?php print render($action_links); ?></ul>
+    <?php endif; ?>
+
+    <?php if (isset($tabs['#primary'][0]) || isset($tabs['#secondary'][0])): ?>
+      <nav class="tabs"><?php print render($tabs); ?></nav>
+    <?php endif; ?>
+
+    <?php if($page['highlighted'] OR $messages){ ?>
+      <div class="drupal-messages">
+      <?php print render($page['highlighted']); ?>
+      <?php print $messages; ?>
+      </div>
+    <?php } ?>
+
+
+    <?php print render($page['content_pre']); ?>
+
+    <?php print render($page['content']); ?>
+
+    <?php print render($page['content_post']); ?>
+
+  </div><!-- /main-->
+
+
+</div><!-- /page-->
+
+
